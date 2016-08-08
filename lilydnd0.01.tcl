2a85c3925bc2ed55aa10d813a03845c126e78228
Lily Star <lily@disorg.net>
2016-08-07 21:54:52 -0700

    basic char listing

--- lilydnd0.01.tcl
+++ lilydnd0.01.tcl
@@ -8,8 +8,6 @@
 # The channel would be +moderated and only players who have a character set up can !join or !leave the game, being set +v and -v when they do.
 
 ### Settings ###
-# Set this to f allow friends in the bot to use dndrpg (+f flag)
-# Set this to - if you want everyone to be able to.
 set dndrpg(flags) "-|-"
 
 
@@ -31,8 +29,8 @@ if {![file exists $dnddbfile]} {
 }
 
 
-bind pub $dndrpg(flags) !setup checkdndrpg
-proc checkdndrpg {nick uhost hand chan text} {
+bind pub $dndrpg(flags) !setup setupchar
+proc setupchar {nick uhost hand chan text} {
   if {channel get $chan ldnd} {
     global dndrpg dnddbfile
     sqlite3 ddb $dnddbfile
@@ -42,22 +40,26 @@ proc checkdndrpg {nick uhost hand chan text} {
   }
 }
 
-bind pub $dndrpg(flags) !list dndrpgstats
-proc dndrpgstats {nick uhost hand chan text} {
+bind pub $dndrpg(flags) !list listchar
+proc listchar {nick uhost hand chan text} {
   if {channel get $chan ldnd} {
     global dndrpg dnddbfile
     sqlite3 ddb $dnddbfile
     if {[string match "" $text]} {
-      foreach {charname race level} [ddb eval{SELECT charname, race, level FROM ldndrpg WHERE player=$uhost}] {
+      ddb eval {SELECT charname, race, level FROM ldndrpg WHERE player=$uhost} {
         puthelp "PRIVMSG $nick :Name: \002$charname\002  Race: $race  Level: $level"
       }
     } else {
       set word [string trim $text]
       set charcheck [ddb eval{SELECT player FROM ldndrpg WHERE charname=$word}]
-
-
-
-      puthelp "PRIVMSG $nick :"
+      if ([string match $charcheck $uhost]) {
+        ddb eval {SELECT * FROM ldndrpg WHERE charname=$word} {
+          puthelp "PRIVMSG $nick :Name: \002$charname\002  Race: $race  Alignment: $align"
+          puthelp "PRIVMSG $nick :ExP: $exp  Level: $level  HP: $hp  AC: $ac"
+          puthelp "PRIVMSG $nick :Str: $str Con: $con Dex: $dex Int: $int Wis: $wis Cha: $cha"
+          puthelp "PRIVMSG $nick :Items: $items"
+        }
+      }
     }
     ddb close
   }
